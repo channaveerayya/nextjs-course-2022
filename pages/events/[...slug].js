127b5a0101947ffb10668a996c2c702c85db0e9f
channaveerayya <channucs0009@gmail.com>
2022-05-10 10:26:22 +0530

    project setup

---
+++ pages/events/[...slug].js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function FilteredEventsPage() {
+  return <div>FilteredEventsPage</div>;
+}
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,6 @@
+import React from "react";
+function EventDetailPage() {
+  return <div>EventDetailPage</div>;
+}
+
+export default EventDetailPage;
